6386edc3e9601c08a8a27b4cbe157eec472e4fdc
unreal998 <31666287+unreal998@users.noreply.github.com>
2017-11-28 22:33:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/tasks', function(req, res){
+res.send('Нету задач на сегодня');
+});
+
+app.get('/tasks/:id', function(req, res){
+	res.send(
+		'Вы хотите получить задачу с Id='+
+		req.params.id +
+		'но сегодня задач нет');
+});
+
+app.post('/tasks', function(req, res) {
+  res.send(
+    'Ви хочете створити задачу з ім\'ям "' +
+    req.body.name +
+    '", але на сьогодні задач не було і не буде!'
+  );
+});
+
+//res.sendStatus(201);
+//res.send({ name: 'Quests'});
+app.use('/', express.static(__dirname + '/static'));
+var arr = [ 
+ res.send({name:'/task'})
+];
+app.listen(3000);
+
