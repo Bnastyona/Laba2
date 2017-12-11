8cff5b04f63cd8f91d856c6ccab0302f8819b9f5
unreal998 <31666287+unreal998@users.noreply.github.com>
2017-12-11 23:03:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
+var tasks = [];
 app.use(bodyParser.json());
 
 app.get('/tasks', function(req, res){
-res.send('Нету задач на сегодня');
+  res.send(tasks);
 });
 
-app.get('/tasks/:id', function(req, res){
-	res.send(
-		'Вы хотите получить задачу с Id='+
-		req.params.id +
-		'но сегодня задач нет');
-});
+// app.get('/tasks/:id', function(req, res){
+// 	res.send(
+// 		'Вы хотите получить задачу с Id='+
+// 		req.params.id +
+// 		'но сегодня задач нет');
+// });
 
 app.post('/tasks', function(req, res) {
-  res.send(
-    'Ви хочете створити задачу з ім\'ям "' +
-    req.body.name +
-    '", але на сьогодні задач не було і не буде!'
-  );
+	tasks.push(req.body);
+	res.send('');
 });
 
 //res.sendStatus(201);
 //res.send({ name: 'Quests'});
 app.use('/', express.static(__dirname + '/static'));
 
-var arr = [ 
- app.get('/tasks',function(req,res){
- res.send(taskName);
-})
-];
+// var NameTasks = [ 
+//  app.get('/tasks/:id',function(req,res){
+//  res.send('/tasks');
+//  req.body.tasks
+// })
+// ];
 app.listen(3000);
-
+	
